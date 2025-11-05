d542df92a53d9cf77c134b14420b5affcf055c29
Narwit Kung <oilxdcggg@gmail.com>
2025-11-05 12:58:48 +0700

    practics 1 30/30  commit

--- lib/dart_week2.dart
+++ lib/dart_week2.dart
@@ -69,4 +69,137 @@ void main() {
 
   print("ข้อที่.17");
   print("Score: ${score != 100}");
+
+  print("ข้อ 17 ");
+  print(score != 100);
+
+  print("\n");
+
+  var isMember = true;
+  var hasCoupon = false;
+  print("ข้อ 18 ");
+  print(isMember && hasCoupon);
+
+  print("\n");
+
+  print("ข้อ 19 ");
+  print(isMember || hasCoupon);
+
+  print("\n");
+
+  print("ข้อ 20 ");
+  print(!hasCoupon);
+
+  print("\n");
+
+  print("ข้อ 21 ");
+  int age2 = 25;
+  if (age2 >= 18) {
+    print("You are an adult.");
+  }
+
+  print("\n");
+
+  print("ข้อ 22 ");
+  int number = -5;
+  if (number > 0) {
+    print("Positive");
+  } else {
+    print("Negative or Zero");
+  }
+
+  print("\n");
+
+  print("ข้อ 23 ");
+  for (int i = 1; i <= 5; i++) {
+    print("วน $i");
+  }
+
+  print("\n");
+
+  print("ข้อ 24 ");
+  int count = 1;
+  while (count <= 3) {
+    print("นับ $count");
+    count++;
+  }
+
+  print("\n");
+
+  print("ข้อ 25 ");
+  int score2 = 80;
+  String result = (score >= 60) ? "Pass" : "Fail";
+  print(result);
+
+  print("\n");
+
+  print("ข้อ 26 ");
+  int day = 3;
+  switch (day) {
+    case 1:
+      print("Monday");
+      break;
+    case 2:
+      print("Tuesday");
+      break;
+    case 3:
+      print("Wednesday");
+      break;
+    case 4:
+      print("Thursday");
+      break;
+    case 5:
+      print("Friday");
+      break;
+    case 6:
+      print("Saturday");
+      break;
+    case 7:
+      print("Sunday");
+      break;
+    default:
+      print("Invalid day");
+  }
+
+  print("\n");
+
+  print("ข้อ 27 ");
+  for (int i = 1; i <= 10; i++) {
+    if (i == 4) {
+      print("4 แล้วหยุด");
+      break;
+    }
+    print("i = $i");
+  }
+
+  print("\n");
+
+  print("ข้อ 28 ");
+  for (int i = 1; i <= 5; i++) {
+    if (i == 3) {
+      print("ข้าม 3");
+      continue;
+    }
+    print("i = $i");
+  }
+
+  print("\n");
+
+  print("ข้อ 29 ");
+  int temp = 28;
+  if (temp > 30) {
+    print("It's hot.");
+  } else if (temp >= 20) {
+    print("It's warm.");
+  } else {
+    print("It's cold.");
+  }
+
+  print("\n");
+
+  print("ข้อ 30 ");
+  int number2 = 5;
+  do {
+    print("number2");
+  } while (number2 < 5);
 }
--- test/dart_week2_test.dart
+++ test/dart_week2_test.dart
@@ -1,8 +1,8 @@
-import 'package:dart_week2/dart_week2.dart';
-import 'package:test/test.dart';
+// import 'package:dart_week2/dart_week2.dart';
+// import 'package:test/test.dart';
 
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
+// void main() {
+//   test('main', () {
+//     expect(main(), 42);
+//   });
+// }
